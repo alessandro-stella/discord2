e164fe1de9d483c8729e09d15f5ca9eaf7aba6e2
Alessandro Stella <alessandro.stella2004@gmail.com>
2022-10-12 14:00:46 +0200

    Adding loader

--- components/CustomLoader.jsx
+++ components/CustomLoader.jsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 export default function CustomLoader() {
     return (
         <div className="relative w-full h-full max-h-[20em] max-w-[20em] grid place-content-center">
-            <div className="h-1/2 relative min-h-[10em]">
-                <Image src="/loader.svg" layout="fill" />
-            </div>
-            <div className="w-40 h-2 bg-discordGrey-200 shadow-xl mx-auto rounded-full"></div>
+            <div
+                className="h-1/2 squared relative min-h-[10em]  bg-center bg-no-repeat"
+                style={{ backgroundImage: "url('/loader.svg')" }}></div>
+            <div className="w-40 h-2 mx-auto rounded-full shadow-xl bg-discordGrey-200"></div>
+            <div className="text-white text-center font-medium p-2 text-xl">Loading...</div>
         </div>
     );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
+import CustomLoader from "components/CustomLoader";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function redirect() {
+export default function redirect({ randomIndex }) {
     const router = useRouter();
     const { data: session, status } = useSession();
 
@@ -14,5 +15,19 @@ export default function redirect() {
     }, [session, status]);
     useEffect(() => {});
 
-    return <div>Loading...</div>;
+    return (
+        <div
+            className="relative grid w-screen h-screen bg-center bg-no-repeat place-items-center"
+            style={{ backgroundImage: `url('/bg${randomIndex}.png')` }}>
+            <div className="bg-discordGrey-700 p-4 shadow-xl rounded-md  m-4 min-w-[30em] w-[80%] max-w-[50em] min-h-[20em] h-[70%] max-h-[50em] grid place-items-center">
+                <CustomLoader />
+            </div>
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    return {
+        props: { randomIndex: Math.floor(Math.random() * 5 + 1) },
+    };
 }
