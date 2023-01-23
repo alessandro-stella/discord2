311e2a5b8d8e5d9e2b3ca1288ae76e96f8b956f5
Alessandro Stella <alessandro.stella2004@gmail.com>
2023-01-23 01:02:44 +0100

    Minor changes

--- components/home/GuildChannels.jsx
+++ components/home/GuildChannels.jsx
@@ -19,7 +19,7 @@ export default function GuildChannels({
     channels,
     deleteGuild,
 }) {
-    const [showOptions, setShowOptions] = useState(true);
+    const [showOptions, setShowOptions] = useState(false);
 
     useEffect(() => setShowOptions(false), [guildId]);
 
